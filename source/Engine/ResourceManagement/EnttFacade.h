5cbbd5a3117d3de0a1fd95c0e062ceb35ba7cdea
Shaunzy <68426146+LightStrike-Zero@users.noreply.github.com>
2025-04-09 22:56:36 +0800

    Removed enttfacade cpp as its a templated class

--- source/Engine/ResourceManagement/EnttFacade.h
+++ source/Engine/ResourceManagement/EnttFacade.h
@@ -12,35 +12,29 @@ public:
     using Entity = entt::entity;
     using Registry = entt::registry;
 
-    // Create an entity.
     Entity createEntity() {
         return m_registry.create();
     }
 
-    // Destroy an entity.
     void destroyEntity(Entity entity) {
         m_registry.destroy(entity);
     }
 
-    // Templated method: add a component to an entity.
     template<typename Component, typename... Args>
     Component& addComponent(Entity entity, Args&&... args) {
         return m_registry.emplace<Component>(entity, std::forward<Args>(args)...);
     }
 
-    // Templated method: get a component from an entity.
     template<typename Component>
     Component& get(Entity entity) {
         return m_registry.get<Component>(entity);
     }
 
-    // Templated method: get a view for components.
     template<typename... Components>
     auto view() {
         return m_registry.view<Components...>();
     }
 
-    // Optionally add other helper functions (e.g., removeComponent, hasComponent, etc.)
     template<typename Component>
     void removeComponent(Entity entity) {
         m_registry.remove<Component>(entity);
@@ -52,7 +46,6 @@ public:
         return m_registry.all_of<Component>(entity);
     }
 
-    // Expose the underlying registry if necessary (but try to keep usage internal).
     Registry& getRegistry() { return m_registry; }
 
 private:
