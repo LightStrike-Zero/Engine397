1b12744f2768357cba57b80f7dc453fca4d1116e
Bukoth <78716213+BuketSwain@users.noreply.github.com>
2025-04-11 01:44:38 +0800

    Doxygen added to OpenGL folder

--- source/Engine/OpenGL/OpenGLMeshBuffer.h
+++ source/Engine/OpenGL/OpenGLMeshBuffer.h
@@ -2,26 +2,56 @@
 // Created by Shaun on 7/03/2025.
 //
 
+/**
+ * @file OpenGLMeshBuffer.h
+ * @brief Defines the OpenGL implementation, inherits from IMeshBuffer for managing mesh data on the GPU.
+ * @author Shaun
+ * @date 2025-03-07
+ */
+
 #ifndef OPENGLMESHBUFFER_H
 #define OPENGLMESHBUFFER_H
 #include "Components/RenderableComponent.h"
 #include "Interfaces/IMeshBuffer.h"
 
+/**
+ * @class OpenGLMeshBuffer
+ * @brief Represents GPU-side mesh buffer: OpenGL Vertex Array, Vertex Buffer, and Element Buffer.
+ */
 class OpenGLMeshBuffer : public IMeshBuffer
 {
 public:
+    /**
+     * @brief Constructs an OpenGL mesh buffer.
+     * @param numIndices Number of indices in the index buffer.
+     * @param numVertices Number of vertices in the vertex buffer.
+     * @param indexData Pointer to the array of indices.
+     * @param vertexData Pointer to the array of vertex data.
+     */
     OpenGLMeshBuffer(int numIndices, int numVertices, std::vector<unsigned int>::pointer indexData, std::vector<Vertex>::pointer vertexData);
 
+    /**
+     * @brief Binds the VAO for rendering.
+     */
     void bind() override;
 
-    // move this later
+    /**
+     * @brief Draws the mesh using the bound VAO.
+     */
     void draw() override;
 
+    /**
+     * @brief Destructor that cleans up GPU resources.
+     */
     ~OpenGLMeshBuffer();
 
+    /**
+     * @brief Retrieves the VAO ID.
+     * @return The OpenGL VAO ID.
+     */
     unsigned int getVAO() const { return m_vao; }
 
-// private:
+// private: //TODO Does this stay private?
     unsigned int m_vao, m_vbo, m_ebo;
     bool m_lineMode;
     size_t m_indexCount;
--- source/Engine/OpenGL/OpenGLQuadBuffer.h
+++ source/Engine/OpenGL/OpenGLQuadBuffer.h
@@ -2,20 +2,51 @@
 // Created by Shaun on 15/03/2025.
 //
 
+/**
+ * @file OpenGLQuadBuffer.h
+ * @brief Declares a buffer specifically for rendering 2D screen-space quads using OpenGL.
+ * @author Shaun
+ * @date 2025-03-15
+ */
+
 #ifndef OPENGLQUADBUFFER_H
 #define OPENGLQUADBUFFER_H
 
 #include "Interfaces/IDataBuffer.h"
 
+/**
+ * @class OpenGLQuadBuffer
+ * @brief An OpenGL implementation of a data buffer used for rendering fullscreen or 2D quads.
+ */
 class OpenGLQuadBuffer : public IDataBuffer
 {
 public:
+    /**
+     * @brief Constructor. Initializes VAO and VBO for the quad.
+     */
     OpenGLQuadBuffer();
+
+    /**
+     * @brief Destructor. Cleans up OpenGL resources.
+     */
     ~OpenGLQuadBuffer() override;
 
+
+    /**
+     * @brief Binds the buffer to the current OpenGL context.
+     */
     void bind() override;
+
+    /**
+     * @brief Unbinds the buffer from the current OpenGL context.
+     */
     void unbind() override;
+
+    /**
+     * @brief Renders the quad using the currently bound shader and state.
+     */
     void render() const;
+
 private:
     unsigned int m_vao, m_vbo;
     
