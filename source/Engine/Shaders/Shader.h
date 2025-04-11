e235d0a0862289ca519a4f1b9e128215e42b693c
Bukoth <78716213+BuketSwain@users.noreply.github.com>
2025-04-11 16:48:44 +0800

    additional Doxygen added to Shader folder

--- source/Engine/Shaders/Shader.h
+++ source/Engine/Shaders/Shader.h
@@ -3,6 +3,17 @@
 // modified by Shaun on 30/03/2025
 //
 
+/**
+ * @file Shader.h
+ * @brief Defines the Shader class for compiling and managing GLSL shaders.
+ *
+ * This class provides functionality to load, compile, bind, and manage
+ * uniform variables in vertex and fragment shaders.
+ *
+ * @author Shaun
+ * @date Created: 2024-10-05
+ * @date Modified: 2025-03-30
+ */
 
 #ifndef SHADER_H
 #define SHADER_H
@@ -10,30 +21,58 @@
 #include <unordered_map>
 #include <glm/fwd.hpp>
 
+/**
+ * @class Shader
+ * @brief Encapsulates an OpenGL shader program and its uniforms.
+ */
 class Shader
 {
 private:
-    std::string m_VSFilePath;
-    std::string m_FSFilePath;
-    unsigned int m_shaderID;
-    std::unordered_map<std::string, int> m_UniformLocationCache;
+    std::string m_VSFilePath;                                            ///< Path to vertex shader
+    std::string m_FSFilePath;                                            ///< Path to fragment shader
+    unsigned int m_shaderID;                                             ///< OpenGL shader program ID
+    std::unordered_map<std::string, int> m_UniformLocationCache;         ///< Cache for uniform locations
+
 public:
+    /**
+     * @brief Constructs a Shader object from given vertex and fragment shader file paths.
+     */
     Shader::Shader(const std::string& vs_filepath, const std::string& fs_filepath);
+
+    /**
+     * @brief Destructor that deletes the shader program.
+     */
     ~Shader();
 
+    /// @brief Binds the shader program for use.
     void Bind() const;
+
+    /// @brief Unbinds the shader program.
     void Unbind() const;
 
+    /// @brief Sets a float uniform.
     void SetUniform1f(const std::string& name, float value);
+
+    /// @brief Sets an integer uniform.
     void SetUniform1i(const std::string& name, int value);
+
+    /// @brief Sets a 2D float vector uniform.
     void SetUniform2f(const std::string& name, const glm::vec2& value);
+
+    /// @brief Sets a 3D float vector uniform.
     void SetUniform3f(const std::string& name, const glm::vec3& value);
+
+    /// @brief Sets a 4-component float uniform.
     void SetUniform4f(const std::string& name, float v0, float v1, float v2, float v3);
+
+    /// @brief Sets a 4x4 matrix uniform.
     void SetUniformMat4f(const std::string& name, const glm::mat4& matrix);
 
+    /// @brief Returns the internal OpenGL shader ID.
     unsigned int GetShaderID() const { return m_shaderID; }
 
     //TODO fix this - very dirty
+    /// @brief (Temporary) Path to shaders, currently hardcoded.
     std::string shaderDir = "source/Engine/Shaders/";
 
 private:
@@ -42,7 +81,5 @@ private:
     unsigned int CreateShader(const std::string& vertexShader, const std::string& fragmentShader);
     std::string ParseShader(const std::string& filepath);
 };
-
-
 
 #endif //SHADER_H
--- source/Engine/Shaders/ShaderManager.h
+++ source/Engine/Shaders/ShaderManager.h
@@ -2,6 +2,17 @@
 // Created by Shaun on 10/02/2025.
 //
 
+/**
+ * @file ShaderManager.h
+ * @brief Manages and stores shaders used throughout the application.
+ *
+ * Provides functionality to load and retrieve reusable shader programs,
+ * wrapping OpenGLShader objects.
+ *
+ * @author Shaun
+ * @date Created: 2025-02-10
+ */
+
 #ifndef SHADERMANAGER_H
 #define SHADERMANAGER_H
 #include <memory>
@@ -9,21 +20,35 @@
 
 #include "OpenGL/OpenGLShader.h"
 
-
+/**
+ * @class ShaderManager
+ * @brief Central manager for loading and accessing OpenGL shaders.
+ */
 class ShaderManager {
+
 private:
     std::unordered_map<std::string, std::shared_ptr<OpenGLShader>> m_shaders;
+
 public:
+    /**
+     * @brief Retrieves a shader by name.
+     * @param shaderName The name used to store the shader.
+     * @return A shared pointer to the OpenGLShader.
+     */
     std::shared_ptr<OpenGLShader> getShader(const std::string& shaderName) {
         return m_shaders.at(shaderName);
     }
 
+    /**
+     * @brief Loads and stores a shader from given file paths.
+     * @param shaderName The name to associate with the shader.
+     * @param vertexPath Path to the vertex shader file.
+     * @param fragmentPath Path to the fragment shader file.
+     */
     void loadShader(const std::string& shaderName, const std::string& vertexPath, const std::string& fragmentPath)
     {
         m_shaders.emplace(shaderName, std::make_shared<OpenGLShader>(vertexPath, fragmentPath));
     }
 };
-
-
 
 #endif //SHADERMANAGER_H
